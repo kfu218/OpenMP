6c8a55996cef5664dee039d959865500553d0b06
fatbulat <fatbulat5@gmail.com>
2015-09-26 21:33:30 +0300

    Correct second exercise

--- theme 1/ex2.c
+++ theme 1/ex2.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <omp.h>
 
+
+void run_if_multi_threads(int threads_count)
+{
+    omp_set_num_threads(threads_count);
+    printf("You set threads count eql %d\n", threads_count);
+    #pragma omp parallel if (threads_count > 1)
+    if (omp_in_parallel())
+    {
+        printf("multi threads: threads count = %d; current threas number = %d\n", omp_get_num_threads(), omp_get_thread_num());
+    }
+}
+
 int main()
 {
-    #pragma omp parallel if (omp_get_num_threads() > 1)
-    if (omp_in_parallel())
-    {
-        printf("threads count = %d; current threas number = %d\n", omp_get_num_threads(), omp_get_thread_num());
-    }
-    #pragma omp parallel if (omp_get_num_threads() == 1)
-    if (omp_in_parallel())
-    {
-        printf("threads count = %d; current threas number = %d\n", omp_get_num_threads(), omp_get_thread_num());
-    }
+    run_if_multi_threads(3);
+    run_if_multi_threads(1);
     return 0;
 }
