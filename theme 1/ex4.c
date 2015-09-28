72025a1ab80c5574e36509745ca306dd7e113a52
fatbulat <fatbulat5@gmail.com>
2015-09-28 10:42:09 +0300

    Implement 4th exercise

--- theme 1/ex4.c
+++ theme 1/ex4.c
@@ -11,11 +11,40 @@ void read(int array[], int size)
 
 int main()
 {
-    const int N = 2;
+    const int N = 10;
     int a[N], b[N];
+    int min, max;
 
     read(a, N);
     read(b, N);
+
+    #pragma omp parallel
+    {
+        if (omp_get_thread_num() == 0)
+        {
+            min = a[0];
+            for(int i = 0; i < N; i++)
+            {
+                if (a[i] < min)
+                    min = a[i];
+            }
+        }
+    }
+
+    #pragma omp parallel
+    {
+        if (omp_get_thread_num() == 1)
+        {
+            max = b[0];
+            for(int i = 0; i < N; i++)
+            {
+                if (b[i] > max)
+                    max = b[i];
+            }
+        }
+    }
+
+    printf("a min = %d; b max = %d\n", min, max);
 
     return 0;
 }
